91a875ab79f2ec0007903c51f71c1c0c5929c469
Matthew Parker <matt_parker@outlook.com>
2026-02-18 10:53:50 -0500

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+// Lawnchair Lite v0.8.0 - Project Build
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
