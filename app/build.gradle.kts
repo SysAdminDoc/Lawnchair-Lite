f59885e935eaad71c2a44699df561a7d822d3d3f
Matthew Parker <matt_parker@outlook.com>
2026-03-28 05:11:13 -0400

    v2.20.0: bug fixes, UI polish, swipe-down settings dismiss, default icon shape

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "app.lawnchairlite"
         minSdk = 28
         targetSdk = 34
-        versionCode = 47
-        versionName = "2.19.0"
+        versionCode = 48
+        versionName = "2.20.0"
     }
 
     signingConfigs {
